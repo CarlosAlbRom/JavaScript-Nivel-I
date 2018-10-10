5669b68ee99e148df3557486cad1e8e46afdc8ab
cadif1 <cadif1@hotmail.com>
2018-10-10 13:28:52 -0300

    Fin de la Clase 3

---
+++ Clase 3/Clase3_2.js
@@ -0,0 +1,4 @@
+function Jojo()
+{
+	alert("Jojojojojo")
+}
---
+++ Clase 3/Clase3_3.js
@@ -0,0 +1,19 @@
+var x = 0
+
+function Prueba()
+{	
+	x++
+	if (x==1) 
+	{
+		alert("Haz Pulsado el Boton 1 Vez")
+	}
+	else 
+	{
+		alert("Haz Pulsado este Boton " + x + " Veces")
+	}
+	
+}
+function Dude()
+{
+	alert("Marico el que lo LEA\nTe dije que no lo Pulsaras")
+}
